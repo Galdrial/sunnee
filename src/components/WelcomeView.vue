a5c48731679b9722550e5bf581d027b68c388380
Galdrial <camerano.simo@gmail.com>
2026-01-12 13:56:57 +0100

    feat: update text content in ThanksView and WelcomeView, adjust styles in base.css

--- src/components/WelcomeView.vue
+++ src/components/WelcomeView.vue
@@ -9,7 +9,7 @@ import BottlePreview from './BottlePreview.vue';
     <section>
       <h1>Welcome to the Sunnee Kids Bottle Configurator!</h1>
       <p class="relative w-100 h-30 text-2xl pb-0 pt-30 left-70">
-        <span role="img" aria-label="wave">🌊</span> Create your own sustainable thermal bottle and personalize it with your favorite colors. With your first order, you’ll receive a unique bottle designed to protect the sea and our planet.<br><br>
+        Create your own sustainable thermal bottle and personalize it with your favorite colors. With your first order, you’ll receive a unique bottle designed to protect the sea and our planet.<br><br>
         Together, we can turn plastic waste into a valuable resource. Start designing your bottle now every choice makes a difference!
       </p>
     </section>
